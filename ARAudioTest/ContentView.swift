e96b598250730838db24157edc808fb7e9b7e756
ada10086 <qj252@nyu.edu>
2019-08-07 17:18:53 -0400

    created EntityData class & entityDataArray for hitTest

--- ARAudioTest/ContentView.swift
+++ ARAudioTest/ContentView.swift
@@ -42,8 +42,8 @@ struct ARViewContainer: UIViewRepresentable {
     
     //called when anything inside is changed
     func updateUIView(_ uiView: ARViewTest, context: Context) {
-//      uiView.loopAudio(loopOn)
-//        uiView.playStopAudio(isPlaying)
+        //      uiView.loopAudio(loopOn)
+        //        uiView.playStopAudio(isPlaying)
     }
     
 }
@@ -52,11 +52,14 @@ class ARViewTest: ARView {
     /// assets
     var audio1: AudioFileResource?
     var audio2: AudioFileResource?
-    var box: Entity!
-    var capsule: Entity!
+    var boxEntity: Entity!
+    var capsuleEntity: Entity!
     let myView = UIView(frame: CGRect(x: 100, y: 100, width: 300, height: 300))
     var audioPlaybackController: AudioPlaybackController?
+    var entityDataArray: [EntityData] = []
     
+    var boxEntityData: EntityData!
+    var capsuleEntityData: EntityData!
     /// init
     override init(frame frameRect: CGRect, cameraMode: ARView.CameraMode, automaticallyConfigureSession: Bool) {
         super.init(frame: frameRect)
@@ -69,39 +72,45 @@ class ARViewTest: ARView {
         // Add the box anchor to the scene
         self.scene.anchors.append(boxAnchor)
         
-        box = self.scene.findEntity(named: "Steel Box")
+        boxEntity = self.scene.findEntity(named: "Steel Box")
         
         //add CollisionComponent for hitTest
-        box.components[CollisionComponent] = CollisionComponent(shapes: [.generateBox(size: [0.1,0.1,0.1])])
-
-        capsule = self.scene.findEntity(named: "capsule")
-        capsule!.setPosition([1,0,0], relativeTo: nil)
-        capsule!.components[CollisionComponent] = CollisionComponent(shapes: [.generateCapsule(height: 0.2, radius: 0.05)])
+        boxEntity.components[CollisionComponent] = CollisionComponent(shapes: [.generateBox(size: [0.1,0.1,0.1])]) //can get self size???
         
+        
+        capsuleEntity = self.scene.findEntity(named: "capsule")
+        capsuleEntity!.setPosition([1,0,0], relativeTo: nil)
+        capsuleEntity!.components[CollisionComponent] = CollisionComponent(shapes: [.generateCapsule(height: 0.2, radius: 0.05)])//radius vs diameter???
+        
+        
+        boxEntityData = EntityData(entity: boxEntity, audioFileName: "hello.mp3")
+        capsuleEntityData = EntityData(entity: capsuleEntity, audioFileName: "bottle.wav")
+        
+        entityDataArray = [boxEntityData, capsuleEntityData]
         // audio = try! AudioFileResource.load(named: "hello.mp3", inputMode: .spatial, loadingStrategy: .preload, shouldLoop: true)
         // audioPlaybackController = box!.prepareAudio(self.audio)
-
-        //loadAsync
-        let loadRequest1 = AudioFileResource.loadAsync(named: "hello.mp3", inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-        let observer1 = Subscribers.Sink<AudioFileResource, Error>(receiveCompletion: { completion in
-            print("completed")
-        }, receiveValue: { audio in
-            // Use loaded audio here
-            self.audioPlaybackController = self.box!.prepareAudio(audio)
-            self.audio1 = audio
-        })
-        loadRequest1.subscribe(observer1)
         
-                let loadRequest2 = AudioFileResource.loadAsync(named: "bottle.wav", inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-                let observer2 = Subscribers.Sink<AudioFileResource, Error>(receiveCompletion: { completion in
-                    print("completed")
-                }, receiveValue: { audio in
-                    // Use loaded audio here
-//                    self.audioPlaybackController = self.box!.prepareAudio(audio)
-        //            self.audioPlaybackController!.play()
-                    self.audio2 = audio
-                })
-                loadRequest2.subscribe(observer2)
+//        //loadAsync
+//        let loadRequest1 = AudioFileResource.loadAsync(named: "hello.mp3", inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+//        let observer1 = Subscribers.Sink<AudioFileResource, Error>(receiveCompletion: { completion in
+//            print("completed")
+//        }, receiveValue: { audio in
+//            // Use loaded audio here
+//            self.audioPlaybackController = self.boxEntity!.prepareAudio(audio)
+//            self.audio1 = audio
+//        })
+//        loadRequest1.subscribe(observer1)
+//
+//        let loadRequest2 = AudioFileResource.loadAsync(named: "bottle.wav", inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+//        let observer2 = Subscribers.Sink<AudioFileResource, Error>(receiveCompletion: { completion in
+//            print("completed")
+//        }, receiveValue: { audio in
+//            // Use loaded audio here
+//            //            self.audioPlaybackController = self.box!.prepareAudio(audio)
+//            //            self.audioPlaybackController!.play()
+//            self.audio2 = audio
+//        })
+//        loadRequest2.subscribe(observer2)
         
         
         let tap = UITapGestureRecognizer(target:self, action: #selector(handleTap(_:)))
@@ -114,51 +123,57 @@ class ARViewTest: ARView {
         let location = sender.location(in: self)
         guard let hitEntity = self.entity(at: location) else { return }
         print("hitEntity: \(hitEntity)")
-        if hitEntity == box {
-            if let audio = audio1 {
-                hitEntity.playAudio(audio)
-            }
-        } else if hitEntity == capsule {
-            if let audio = audio2 {
-                hitEntity.playAudio(audio)
+        let entities = entityDataArray.map({ $0.entity })
+        for (index, entity) in entities.enumerated() {
+            if hitEntity == entity {
+                entityDataArray[index].audioPlaybackController.play()
             }
         }
-//            if let controller = audioPlaybackController {
-////                //if looped, toggle play/stop
-////                if controller.isPlaying {
-////                    controller.stop()
-////                } else {
-////                    controller.play()
-////                }
-//                //if not looped, tap to play once
-//                controller.play()
+//        if hitEntity == boxEntity {
+//            if let audio = audio1 {
+//                hitEntity.playAudio(audio)
 //            }
-        }
-    
-//        func loopAudio(_ loopOn: Bool) {
-//            if let controller = audioPlaybackController {
-//                if controller.isPlaying {
-//                    controller.stop()
-//                }
-//                audio?.shouldLoop = loopOn
-//                controller.play()
-//            }
-//            myView.backgroundColor = loopOn ? .green : .red
-////            print("looping:\(self.audio.shouldLoop)")
-//        }
-    
-//    func playStopAudio(_ isPlaying: Bool){
-//            if let controller = audioPlaybackController {
-//                //if looped, toggle play/stop
-//                if isPlaying {
-//                    controller.play()
-//                } else {
-//                    controller.stop()
-//                }
-//                //if not looped, tap to play once
-//                controller.play()
+//        } else if hitEntity == capsuleEntity {
+//            if let audio = audio2 {
+//                hitEntity.playAudio(audio)
 //            }
 //        }
+        //            if let controller = audioPlaybackController {
+        ////                //if looped, toggle play/stop
+        ////                if controller.isPlaying {
+        ////                    controller.stop()
+        ////                } else {
+        ////                    controller.play()
+        ////                }
+        //                //if not looped, tap to play once
+        //                controller.play()
+        //            }
+    }
+    
+    //        func loopAudio(_ loopOn: Bool) {
+    //            if let controller = audioPlaybackController {
+    //                if controller.isPlaying {
+    //                    controller.stop()
+    //                }
+    //                audio?.shouldLoop = loopOn
+    //                controller.play()
+    //            }
+    //            myView.backgroundColor = loopOn ? .green : .red
+    ////            print("looping:\(self.audio.shouldLoop)")
+    //        }
+    
+    //    func playStopAudio(_ isPlaying: Bool){
+    //            if let controller = audioPlaybackController {
+    //                //if looped, toggle play/stop
+    //                if isPlaying {
+    //                    controller.play()
+    //                } else {
+    //                    controller.stop()
+    //                }
+    //                //if not looped, tap to play once
+    //                controller.play()
+    //            }
+    //        }
     
     
     @available(*, unavailable)
@@ -182,3 +197,43 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+
+//struct(Escaping closure captures mutating 'self' parameter)
+//vs class('self' captured by a closure before all members were initialized)
+class EntityData {
+    var entity: Entity!
+    var audioFileName: String! //URL
+    var audio: AudioFileResource
+    var audioPlaybackController: AudioPlaybackController
+    var audioShouldLoop: Bool = false
+    
+    init(entity: Entity, audioFileName: String) {
+        self.entity = entity
+        self.audioFileName = audioFileName
+        //initialize first
+        audio = try! AudioFileResource.load(named: "piano.wav", inputMode: .spatial, loadingStrategy: .preload, shouldLoop: true)
+        audioPlaybackController = entity.prepareAudio(audio)
+        
+        let loadRequest = AudioFileResource.loadAsync(named: audioFileName, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: audioShouldLoop)
+        let observer = Subscribers.Sink<AudioFileResource, Error>(receiveCompletion: { completion in
+            print("completed")
+        }, receiveValue: { loadedAudio in
+            self.audio = loadedAudio
+            self.audioPlaybackController = self.entity.prepareAudio(loadedAudio)
+        })
+        loadRequest.subscribe(observer)
+    }
+    
+//    func loadAudioFile(){
+//        let loadRequest = AudioFileResource.loadAsync(named: audioFileName, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: audioShouldLoop)
+//        let observer = Subscribers.Sink<AudioFileResource, Error>(receiveCompletion: { completion in
+//            print("completed")
+//        }, receiveValue: { loadedAudio in
+//            self.audio = loadedAudio
+//            self.audioPlaybackController = self.entity.prepareAudio(loadedAudio)
+//        })
+//        loadRequest.subscribe(observer)
+//    }
+    
+}
